55a224b9ef9f4d3941319db87fafb8de087c9c1e
djordje <obradovic.djordje@gmail.com>
2018-11-20 00:04:27 +0100

    file upload

--- src/www/js/app.controller.js
+++ src/www/js/app.controller.js
@@ -1,11 +1,11 @@
 (function () {
     'use strict';
 
-    var app = angular.module('myApp', []);
+    var app = angular.module('myApp', ['ui.bootstrap', 'angularFileUpload']);
     app.controller('AppCtrl', AppController);
 
     // AppController.$inject = ['$rootScope', '$scope', '$http'];
-    function AppController($rootScope, $scope, $http) {
+    function AppController($rootScope, $scope, $http, FileUploader ) {
         var vm = this;
         vm.username = $rootScope.loggedUser;
         vm.password = '';
@@ -25,25 +25,28 @@
                 url: '/api/users/login?username='+vm.username+'&password='+vm.password,
                 headers: {'auth-token': '123'}
             }
-            if(vm.password == '1234' && vm.username =='djordje'){
-                vm.message = "Prijavljen je korisnik: "+vm.username;
-                vm.autorizovan = true;
-            }
-                else if(vm.password == '4321' && vm.username =='petar')
-                {
-                    vm.message = "Prijavljen je korisnik: "+vm.username;
-                    vm.autorizovan = true;
-                }
-              else
-              {
-                vm.message = "Pogresno korisnicko ime i/ili lozinka!";
-              }
-              if(vm.password == '' || vm.username == '')
-                {
-                    alert("Morate uneti korisnicko ime i sifru!");
-                }
+            // if(vm.password == '1234' && vm.username =='djordje'){
+            //     vm.message = "Prijavljen je korisnik: "+vm.username;
+            //     vm.autorizovan = true;
+            // }
+            //     else if(vm.password == '4321' && vm.username =='petar')
+            //     {
+            //         vm.message = "Prijavljen je korisnik: "+vm.username;
+            //         vm.autorizovan = true;
+            //     }
+            //   else
+            //   {
+            //     vm.message = "Pogresno korisnicko ime i/ili lozinka!";
+            //   }
+            //   if(vm.password == '' || vm.username == '')
+            //     {
+            //         alert("Morate uneti korisnicko ime i sifru!");
+            //     }
             $http(req).then(function(resp){
                 console.log(resp)
+                if(resp.data.res == 'success'){
+                    vm.autorizovan = true;
+                }
                 /*vm.message = resp;*/
             }, function(err){
                 vm.message = err;
@@ -90,6 +93,73 @@
            vm.autorizovan = false;
            vm.message = "Hvala sto ste koristili aplikaciju.";
         }
+
+        /******************************************** */
+        vm.uploader = $scope.uploader = new FileUploader({ url: '/api/files/upload/' });
+
+        // a sync filter
+        vm.uploader.filters.push({
+            name: 'syncFilter',
+            fn: function (item /*{File|FileLikeObject}*/, options) {
+                console.log('syncFilter');
+                return this.queue.length < 10;
+            }
+        });
+
+        // an async filter
+        vm.uploader.filters.push({
+            name: 'asyncFilter',
+            fn: function (item /*{File|FileLikeObject}*/, options, deferred) {
+                console.log('asyncFilter');
+                setTimeout(deferred.resolve, 1e3);
+            }
+        });
+
+        // CALLBACKS
+
+        vm.uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        vm.uploader.onAfterAddingFile = function (fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        vm.uploader.onAfterAddingAll = function (addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        vm.uploader.onBeforeUploadItem = function (item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        vm.uploader.onProgressItem = function (fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        vm.uploader.onProgressAll = function (progress) {
+            console.info('onProgressAll', progress);
+        };
+        vm.uploader.onSuccessItem = function (fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        vm.uploader.onErrorItem = function (fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        vm.uploader.onCancelItem = function (fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        vm.uploader.onCompleteItem = function (fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+            var fileName = fileItem._file.name;
+            var d = {
+                'file': fileName
+                }
+            console.log(d);
+        };
+        vm.uploader.onCompleteAll = function () {
+            console.info('onCompleteAll');
+            //vm.init();
+        };
+        /******************************************** */
+
+
+
 
         vm.getUsers();
     }
